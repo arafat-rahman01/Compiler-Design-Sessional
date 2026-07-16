e249b08d28434b44adfe62bd8c34103fb5ca0702
arafat-rahman01 <aabrarbin851@gmail.com>
2026-07-17 00:06:05 +0600

    eliminate white spaces & collect numbers as token

---
+++ wSpcaeToken2.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ifstream file("lex.cpp");
+
+    if(!file.is_open()){
+        cout<<"File not found"<<endl;
+        return 0;
+    }
+
+    char ch;
+
+    while(file.get(ch)){
+        if(!isspace(ch)){
+            cout<<ch;
+        }
+    }
+    cout<<"\n\n";
+
+    file.clear();
+    file.seekg(0);
+
+    while(file.get(ch)){
+        if(isdigit(ch)){
+            string num="";
+            while(isdigit(ch)){
+                num+=ch;
+                if(!file.get(ch)) break;
+            }
+            cout<<"Num\t\t"<<num<<endl;
+        }
+    }
+    file.close();
+}
