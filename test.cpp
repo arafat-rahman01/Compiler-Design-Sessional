0222bd81c10ced36260c84e5d55f39c5a88992c0
arafat-rahman01 <aabrarbin851@gmail.com>
2026-07-24 16:32:20 +0600

    key finding method use

--- test.cpp
+++ test.cpp
@@ -3,38 +3,26 @@ using namespace std;
 int main(){
     ifstream file("lex.cpp");
     if(!file.is_open()){
-        cout<<"File is not open";
+        cout<<"this file is not be open";
         return 0;
     }
+
     string word;
-    
+
     string keyword[]={
-        "int","float","double","if","else","for",
-        "while","return","void","break","continue",
-        "includebitsstdch","using","namespace","std","main"
+        "int","float"  
     };
-
+    
     while(file>>word){
-        string var="";
+        string key="";
         for(char ch:word){
-            if(isalnum(ch) || ch=='_') var+=ch;
+            if(isalnum(ch) || ch=='_') key+=ch;
         }
-
-        if(var=="") continue;
-
-        bool f=false;
-
         for(string k:keyword){
-            if(k==var){
-                f=true;
-                break;
+            if(k==key){
+                cout<<k<<endl;
             }
         }
-
-        if(!f && (isalpha(var[0]) || var[0]=='_')){
-            cout<<var<<endl;
-        }
-
     }
     file.close();
 }
