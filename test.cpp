c972d1aad9f95f235a5a782b76d7da29c4d62148
arafat-rahman01 <aabrarbin851@gmail.com>
2026-07-24 11:52:19 +0600

    testing

--- test.cpp
+++ test.cpp
@@ -1,31 +1,29 @@
 #include<bits/stdc++.h>
-#include<fstream>
-#include<string>
 using namespace std;
 int main(){
-    string keyword[32]={
-        "auto","break","case","char","continue","default",
-        "do","double","else","enum","extern","float","for","goto",
-        "if","int","long","register","return","short","signed",
-        "sizeof","static","struct","switch","typedef","union",
-        "unsigned","void","volatile","while"
-    };
-
     ifstream file("lex.cpp");
-
-    if(!file){
-        cout<<"File not found!";
+    if(!file.is_open()){
+        cout<<"File is not open";
         return 0;
     }
+    char ch;
+    while(file.get(ch)){
+        if(!isspace(ch)){
+            cout<<ch;
+        }
+    }
+    cout<<"\n \n";
 
-    string word;
+    file.clear();
+    file.seekg(0);
 
-    while(file>>word){
-        for(int i=0;i<32;i++){
-            if(word==keyword[i]){
-                cout<<word<<"-->Keyword"<<endl;
-                break;
+    while(file.get(ch)){
+        if(isdigit(ch)){
+            while(isdigit(ch)){
+                cout<<ch;
+                if(!file.get(ch)) break;
             }
+            cout<<endl;
         }
     }
     file.close();
