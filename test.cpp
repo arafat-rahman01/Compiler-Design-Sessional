9e1ef79c655e458c18c8a3692f7107bd8ae9422d
arafat-rahman01 <aabrarbin851@gmail.com>
2026-07-28 02:07:24 +0600

    infix to postfix

--- test.cpp
+++ test.cpp
@@ -159,3 +159,44 @@
 //5)    Design a compiler front-end based on syntax-directed translation 
 //      technique that will function as an infix translator for a language 
 //      consists of sequence of expressions terminated by semicolon
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int priority(char op){
+//     if(op=='+' || op=='-') return 1;
+//     if(op=='*' || op=='/') return 2;
+//     return 0;
+// };
+// int main(){
+//     string infix;
+//     cin>>infix;
+//     stack<char>st;
+
+//     for(char ch:infix){
+//         if(isalnum(ch)) cout<<ch;
+//         else if(ch=='(') st.push(ch);
+//         else if(ch==')'){
+//             while(!st.empty() && st.top()!='('){
+//                 cout<<st.top();
+//                 st.pop();
+//             }
+//             if(!st.empty()) st.pop();
+//         }
+//         else if(ch==';'){
+//             while(!st.empty()){
+//                 cout<<st.top();
+//                 st.pop();
+//             }
+//             break;
+//         }
+//         else{
+//             while(!st.empty() && st.top() !='(' &&
+//                    priority(st.top())>= priority(ch) ){
+//                         cout<<st.top();
+//                         st.pop();
+//                    }
+//                    st.push(ch);
+//         }
+//     }
+//     cout<<endl;
+// }
